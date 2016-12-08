b0e262cb0c24c6a7c1d3dc809e01f3599f7e2180
ivkrpan <ivkrpan@vsite.hr>
2016-12-08 21:18:36 +0100

    gotove StatičkeNestatičkeDelegatskeMetode

--- StatickeNestatickeDelegatskeMetode/StatičkeNestatičkeDelegatskeMetode.cs
+++ StatickeNestatickeDelegatskeMetode/StatičkeNestatičkeDelegatskeMetode.cs
@@ -23,9 +23,12 @@ namespace Vsite.CSharp
         public static void PoziviPrekoDelegata()
         {
             // TODO: Instancirati delegata tipa MojDelegat metodom StatičkaMetoda te pozvati delegata
-
+            MojDelegat md = StatičkaMetoda;
+            md();
             // TODO: Gornjem delegatu pridružiti metodu MetodaInstance te ponovno pozvati delegata
-
+            StatičkeNestatičkeDelegatskeMetode o = new StatičkeNestatičkeDelegatskeMetode();
+            md += o.MetodaInstance;
+            md();
         }
 
         static void Main(string[] args)
